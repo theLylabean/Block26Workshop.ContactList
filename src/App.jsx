52236a64a64226b2b287f88c05badac26cdd2794
Lyla Lynn <lyladlynn@gmail.com>
2025-04-02 22:19:55 -0500

    added css styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
 import { useEffect, useState } from 'react'
 import './index.css'
 import ContactList from './components/ContactList.jsx'
+import SelectedContact from './components/SelectedContact.jsx';
+import './index.css'
+
 function App() {
   const [contacts, setContacts] = useState([]);
+  const [selectedContact, setSelectedContact] = useState(null);
+  const [contact, setContact] = useState(null);
 
   useEffect(() => {
     const getContacts = async () => {
@@ -14,10 +19,26 @@ function App() {
     getContacts();
   }, []);
 
+  // const selectedContactObj = contacts.find(contact => contact.id === selectedContact);
+
   return (
     <>
       <div>
-        <ContactList contacts={contacts} setContacts={setContacts} />
+        {
+          selectedContact
+          ? <SelectedContact id='singleContact'
+          selectedContact={selectedContact} 
+          setSelectedContact={setSelectedContact}
+          contact={contact}
+          setContact={setContact}
+            />
+          : <ContactList id='contactList'
+              contacts={contacts} 
+              setContacts={setContacts} 
+              selectedContact={selectedContact} 
+              setSelectedContact={setSelectedContact} 
+            />
+        }
       </div>
     </>
   )
